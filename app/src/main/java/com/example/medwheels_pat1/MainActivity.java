584e0079d7e636f448b5b4a39efda6b8325432ba
RamanVer1ma <vraman1056@gmail.com>
2024-04-20 01:20:10 +0530

    DropDown menu

--- app/src/main/java/com/example/medwheels_pat1/MainActivity.java
+++ app/src/main/java/com/example/medwheels_pat1/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.medwheels_pat1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +15,50 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    String BloodGroup,Gender,blood_d,gender_d;
     String [] blood_group_array = {"A+","A-","B+","B-","O+","O-","AB+","AB-"};
     String[] gender_array ={"Male","Female","Non-binary","Others"};
+
+AutoCompleteTextView bloodgroupTextView,genderTextView;
+ArrayAdapter<String> bloodgroupItems,genderItems;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        bloodgroupTextView = findViewById(R.id.blood_group_pat);
+
+        bloodgroupItems = new ArrayAdapter<String>(this, R.layout.list_item, blood_group_array);
+        bloodgroupTextView.setAdapter(bloodgroupItems);
+
+        bloodgroupTextView.setAdapter(bloodgroupItems);
+
+       bloodgroupTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
+               blood_d = adapterView.getItemAtPosition(i).toString().trim();
+           }
+       });
+
+
+
+        genderTextView = findViewById(R.id.gender_pat);
+
+        genderItems = new ArrayAdapter<String>(this, R.layout.list_item, gender_array);
+        genderTextView.setAdapter(genderItems);
+
+        genderTextView.setAdapter(genderItems);
+
+        genderTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
+                gender_d = adapterView.getItemAtPosition(i).toString().trim();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
