187f327182690df8c9623b762b22561cfbd6c6f4
therev2 <revanchanna@gmail.com>
2024-04-20 02:50:57 +0530

    added fields in the java class

--- app/src/main/java/com/example/medwheels_pat1/HelperClass.java
+++ app/src/main/java/com/example/medwheels_pat1/HelperClass.java
@@ -1,10 +1,18 @@
 package com.example.medwheels_pat1;
 public class HelperClass {
-    String name, email, password;
-    public HelperClass(String email, String password, String name) {
+    String name, email, password, additionalNotes,phone, emergencyRelation,emergencyName,medHistory,allergies,dob,address;
+    public HelperClass(String email, String password, String name,String additionalNotes,String phone, String emergencyRelation,String emergencyName,String medHistory,String allergies,String dob,String address,String gender,String blood) {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.address=address;
+        this.additionalNotes=additionalNotes;
+        this.phone=phone;
+        this.emergencyName=emergencyName;
+        this.emergencyRelation=emergencyRelation;
+        this.medHistory=medHistory;
+        this.allergies=allergies;
+        this.dob=dob;
     }
     public String getName() {
         return name;
--- app/src/main/java/com/example/medwheels_pat1/MainActivity.java
+++ app/src/main/java/com/example/medwheels_pat1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.medwheels_pat1;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -20,28 +22,31 @@ public class MainActivity extends AppCompatActivity {
     String BloodGroup,Gender,blood_d,gender_d;
     String [] blood_group_array = {"A+","A-","B+","B-","O+","O-","AB+","AB-"};
     String[] gender_array ={"Male","Female","Non-binary","Others"};
+    EditText sname, email, password, additionalNotes,phone, emergencyRelation,emergencyName,medHistory,allergies,dob,address;
     Button finishButton;
-
 AutoCompleteTextView bloodgroupTextView,genderTextView;
 ArrayAdapter<String> bloodgroupItems,genderItems;
 
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        finishButton = findViewById(R.id.finishBtn);
 
-        finishButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,home.class);
-                startActivity(intent);
-            }
-        });
-
+        sname=findViewById(R.id.name_pat);
+        email=findViewById(R.id.email_pat);
+        password=findViewById(R.id.pass_pat);
+        address=findViewById(R.id.cur_add);
+        additionalNotes=findViewById(R.id.additional_notes);
+        phone=findViewById(R.id.phone_rela);
+        emergencyName=findViewById(R.id.emergency_contact_name);
+        emergencyRelation=findViewById(R.id.relation_pat);
+        medHistory=findViewById(R.id.medical_conditions);
+        allergies=findViewById(R.id.allergies);
+        dob=findViewById(R.id.dob);
 
         bloodgroupTextView = findViewById(R.id.blood_group_pat);
 
@@ -72,7 +77,28 @@ ArrayAdapter<String> bloodgroupItems,genderItems;
                 gender_d = adapterView.getItemAtPosition(i).toString().trim();
             }
         });
+        finishButton = findViewById(R.id.finishBtn);
 
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail=email.getText().toString();
+                String name=sname.getText().toString();
+                String pass=password.getText().toString();
+                String addNotes=additionalNotes.getText().toString();
+                String add=address.getText().toString();
+                String Phone=phone.getText().toString();
+                String emName=emergencyName.getText().toString();
+                String emRela=emergencyRelation.getText().toString();
+                String med=medHistory.getText().toString();
+                String all=allergies.getText().toString();
+                String Dob=dob.getText().toString();
+                HelperClass helperClass = new HelperClass(mail,pass,name,addNotes,add,Phone,emName,emRela,med,all,Dob,gender_d,blood_d);
+                Intent intent = new Intent(MainActivity.this,home.class);
+
+                startActivity(intent);
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
